6152fba07a179c092ac070bea1a0d358652da258
RitaPeixoto <rita.peixoto2000@gmail.com>
2020-10-16 16:13:17 +0100

    Finished exercise 5

---
+++ 3. PHP/exercise 2/sum2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Calculator</title>
+    </head>
+
+
+    <body>
+        <h1> Calculator </h1>
+        <h2>
+        <?php echo "Sum = ". ($_GET['num1'] + $_GET['num2']);?>
+        </h2>
+        <h3><a href="form2.html">Return </a></h3>
+    </body>
+
+</html>
