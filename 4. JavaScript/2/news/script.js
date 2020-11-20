c7c0741d58bcc8cdd0d61a95b79b7823f7aa4948
RitaPeixoto <rita.peixoto2000@gmail.com>
2020-11-20 11:57:35 +0000

    Finished exercise 5.2

--- 4. JavaScript/2/news/script.js
+++ 4. JavaScript/2/news/script.js
@@ -1,8 +1,18 @@
 let commentForm = document.querySelector('#comments form')
-
-commentForm.addEventListener('submit', submitContent)
-
-function submitContent(event){
+ 
+commentForm.addEventListener('submit', submitComment)
+ 
+function submitComment(event){
     event.preventDefault()
-    console.log("You have submitted a comment")
+    console.log("You have submited a comment")
+ 
+    let id = document.getElementsByTagName('input')[0].value
+    let comment_id = document.querySelector('#comments article:last-of-type').getAttribute('data-id')
+    let username = document.getElementsByTagName('input')[1].value
+    let text = document.getElementsByTagName('textarea')[0].value
+ 
+ 
+    console.log(id, comment_id, username, text)
+ 
+ 
 }
